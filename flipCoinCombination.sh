68a37dc57e28aca8020c7bb46359ff7e4d8f58f8
Sippora04 <sippora.toppo95@gmail.com>
2020-05-28 14:00:19 +0530

    Modify/Added: CoinFlip_in_DoubletCombination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -46,10 +46,70 @@ echo ${arr[@]}
 max=${arr[2]}
 
 for key in ${!Singlet[@]}
+	do
+		if [ ${Singlet[$key]} -eq $max ]
+			then
+				echo "Winner is $key : ${Singlet[$key]} %"
+				break
+		fi
+	done
+
+
+echo "----DoubletCombination----"
+
+declare -A Doublet
+
+Doublet[HH]=0;
+Doublet[TH]=0
+Doublet[HT]=0;
+Doublet[TT]=0;
+
+for (( i=0; i<$number; i++ ))
 do
-if [ ${Singlet[$key]} -eq $max ]
-then
-echo "Winner is $key : ${Singlet[$key]} %"
-break
-fi
+        random=$(( $RANDOM%4 ))
+        if [ $random -eq 0 ]
+        	then
+         	Doublet[HH]=$(((${Doublet[HH]})+1));
+        		elif [ $random -eq 1 ]
+        			then
+               	 Doublet[HT]=$(((${Doublet[HT]})+1));
+        					elif [ $random -eq 2 ]
+        						then
+                				Doublet[TH]=$(((${Doublet[TH]})+1));
+        							else
+                 					Doublet[TT]=$(((${Doublet[TT]})+1));
+        	fi
+done
+
+count=1;
+for key in ${!Doublet[@]}
+do
+        Doublet[$key]=$(( (${Doublet[$key]} * 100) / $number));
+        arr[count++]=${Doublet[$key]}
+        echo "$key:${Doublet[$key]}"
+done
+
+for (( i = 1; i < 5 ; i++ ))
+	do
+   	for (( j = $i; j < 5; j++ ))
+   		do
+      		if [ ${arr[$i]} -gt ${arr[$j]} ]
+				 then
+           		temp=${arr[$i]}
+           		arr[$i]=${arr[$j]}
+           		arr[$j]=$temp
+     			fi
+   	done
+done
+
+echo ${arr[@]}
+max=${arr[4]}
+
+for key in ${!Doublet[@]}
+	do
+		if [ ${Doublet[$key]} -eq $max ]
+			then
+				echo "Winner is $key : ${Doublet[$key]} %"
+				break
+		fi
 done
