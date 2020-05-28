83075036720ca853f2caa7d350395becf3819736
Sippora04 <sippora.toppo95@gmail.com>
2020-05-28 14:25:21 +0530

    Modify: Store and print Winners

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,6 @@
 echo "Welcome Flip Coin Combination Problem"
 
-echo "Enter a number how many time to flip"
+echo "How many time to flip a Coin:"
 read number
 declare -A Singlet
 
@@ -112,4 +112,77 @@ for key in ${!Doublet[@]}
 				echo "Winner is $key : ${Doublet[$key]} %"
 				break
 		fi
+done
+
+echo "----TripletCombination----"
+
+declare -A Triplet
+Triplet[HHH]=0;
+Triplet[TTT]=0
+Triplet[HHT]=0;
+Triplet[HTH]=0;
+Triplet[THH]=0;
+Triplet[THT]=0
+Triplet[TTH]=0;
+Triplet[HTT]=0;
+
+for (( i=0; i<$number; i++ ))
+do
+        random=$(( $RANDOM%8 ))
+        if [ $random -eq 0 ]
+        then
+            Triplet[HHH]=$(((${Triplet[HHH]})+1));
+        elif [ $random -eq 1 ]
+        then
+            Triplet[TTT]=$(((${Triplet[TTT]})+1));
+        elif [ $random -eq 2 ]
+        then
+             Triplet[HHT]=$(((${Triplet[HHT]})+1));
+        elif [ $random -eq 3 ]
+        then
+             Triplet[HTH]=$(((${Triplet[HTH]})+1));
+        elif [ $random -eq 4 ]
+        then
+             Triplet[THH]=$(((${Triplet[THH]})+1));
+        elif [ $random -eq 5 ]
+        then
+             Triplet[THT]=$(((${Triplet[THT]})+1));
+        elif [ $random -eq 6 ]
+        then
+             Triplet[TTH]=$(((${Triplet[TTH]})+1));
+        else
+             Triplet[HTT]=$(((${Triplet[HTT]})+1));
+        fi
+done
+
+count=1;
+for key in ${!Triplet[@]}
+do
+        Triplet[$key]=$(( (${Triplet[$key]} * 100) / $number));
+        arr[count++]=${Triplet[$key]}
+        echo "$key:${Triplet[$key]}"
+done
+
+for (( i = 1; i < 9 ; i++ ))
+do
+   for (( j = $i; j < 9; j++ ))
+   do
+      if [ ${arr[$i]} -gt ${arr[$j]} ]; then
+           temp=${arr[$i]}
+           arr[$i]=${arr[$j]}
+           arr[$j]=$temp
+     	fi
+   done
+done
+
+echo ${arr[@]}
+max=${arr[8]}
+
+for key in ${!Triplet[@]}
+do
+	if [ ${Triplet[$key]} -eq $max ]
+		then
+		echo "winner is $key : ${Triplet[$key]}%"
+		break
+fi
 done
